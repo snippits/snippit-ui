acf9d99be957fd0d6e3ec86b566892d1f9b5bb68
Medicine Yeh <medicinehy@gmail.com>
2017-10-19 16:35:47 +0800

    Refactor the code module and apply css styles on it

--- src/js/components/CodeResult.jsx
+++ src/js/components/CodeResult.jsx
@@ -18,43 +18,49 @@ export default class CodeResult extends React.Component {
 
     render() {
         const {code} = this.props;
-        const output = [];
-        // This is used to force clean all browser implicit state and rebuild component
-        const codekey = 'phase-' + code.phaseID + '-highlighted-code-' + new Date().getTime();
+        let output = code.data;
 
         console.log('CodeResult');
 
         if (code.error) {
             console.log('Cannot fetch the codes');
-        } else if (code.fetched) {
-            for (let i = 0; i < code.data.length; i++) {
-                output.push({
-                    code: code.data[i].line,
-                });
-            }
+            output = [];
         }
 
+        // This is used to force clean all browser implicit state and rebuild component
+        const codekey = 'phase-' + code.phaseID + '-time-' + new Date().getTime();
         return (
             <div className='col-md-6'>
                 <h1 className='pure-u-1-1'>Code</h1>
-                <HighlightedCode key={codekey} codes={output}/>
+                <HighlightedCode key={codekey} value={output}/>
             </div>
         );
     }
 }
 
 class HighlightedCode extends React.Component {
+    generateTable(values) {
+        if (typeof values === 'string') {
+            return values;
+        } else {
+            return values.map((code, i) =>
+                <tr key={i} className='no-border'>
+                    <td className='col-md-1 walk-count'>{code.walk}</td>
+                    <td className='line-num'>{i} </td>
+                    <td className='code'>{code.line}</td>
+                </tr>
+            );
+        }
+    }
+
     render() {
+        const output = this.generateTable(this.props.value);
+
         return (
             <Highlight className='C++'>
-                <table>
+                <table className='table table-hover table-condensed table-borderless'>
                     <tbody>
-                        {this.props.codes.map(function(object, i) {
-                            return <tr key={i}>
-                                <td>{i} </td>
-                                <td>{object.code}</td>
-                            </tr>;
-                        })}
+                        {output}
                     </tbody>
                 </table>
             </Highlight>
