81aba15d7fc10530520afcb93f6c0c671a6c5612
Medicine Yeh <medicinehy@gmail.com>
2017-10-27 22:31:39 +0800

    Port to Klein for faster speed
    Flask was great but with super slow speed in our RESTful API.
    Requesting timeline could consume 80-160 ms while data are ready
    in 2 ms.
    Using Klein gives the time of about 5 ms in the same HTTP request.

--- scripts/server.py
+++ scripts/server.py
@@ -14,10 +14,10 @@ from argparse import RawDescriptionHelpFormatter
 from functools import partial
 
 # Third party
-import flask
-import flask_profiler
+import klein
 import logzero
 from logzero import logger
+from twisted.web.static import File
 
 # Modules for this project
 from modules import snippit
@@ -34,37 +34,30 @@ BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 PARENTDIR = os.path.dirname(BASE_PATH)
 
 STATIC_DIR = os.path.join(BASE_PATH, '..', 'public')
-DEFAULT_HOST = '127.0.0.1'
-DEFAULT_PORT = 5000
 DEFAULT_INPUT_PATH = '/tmp/snippit'
 
 processes = []
 
 # Set the project root directory as the static folder
-app = flask.Flask(__name__)
+app = klein.Klein()
 
 # Load settings
-app.config.from_object('flask_config.DefaultConfig')
-if os.environ.get('SNIPPIT_UI_CONFIG'):
-    app.config.from_envvar('SNIPPIT_UI_CONFIG')
+setattr(app, 'config', {
+    'DEBUG': False,
+    'HOST': '127.0.0.1',
+    'PORT': 5000,
+})
 
-# Necessary configuration to initialize flask-profiler:
-app.config['flask_profiler'] = {
-    'enabled': app.config['DEBUG'],
-    'storage': {
-        'engine': 'sqlite'
-    },
-    'basicAuth': {
-        'enabled': True,
-        'username': 'admin',
-        'password': 'admin'
-    },
-    'ignore': []
-}
+# Set a minimum log level
+logzero.loglevel(logging.INFO)
+# Set up debug mode settings
+if os.environ.get('DEBUG'):
+    app.config['DEBUG'] = True
+    logzero.loglevel(logging.DEBUG)
 
 
 def get_similarity_params(request):
-    requestValues = request.get_json(silent=True)
+    requestValues = json.loads(request.content.read())
     sim_mat = processes['default_']['similarityMatrix']
     if requestValues['similarityThreshold']:
         sim_thold = float(requestValues['similarityThreshold']) / 100.0
@@ -74,22 +67,23 @@ def get_similarity_params(request):
     return (sim_mat, sim_thold)
 
 
-@app.route('/')
-def root():
-    path = os.path.join(STATIC_DIR, 'index.html')
-    return flask.send_file(path, cache_timeout=0)
+@app.route("/", branch=True)
+def static_file(request):
+    request.setHeader('Cache-Control', 'no-cache')
+    return File(STATIC_DIR)
 
 
-@app.route('/<path:path>')
-def static_file(path):
-    return flask.send_from_directory(STATIC_DIR, path, cache_timeout=0)
+@app.route("/index.html")
+def index(request):
+    request.setHeader('Cache-Control', 'no-cache')
+    return File(os.path.join(STATIC_DIR, 'index.html'))
 
 
 @app.route('/phase/timeline', methods=['POST'])
 @timed
-def get_phase_timeline():
+def get_phase_timeline(request):
     info = processes['default_']['info']
-    (sim_mat, sim_thold) = get_similarity_params(flask.request)
+    (sim_mat, sim_thold) = get_similarity_params(request)
     # Get mapping table for remapping the timeline to its new phase ID
     mapping_table = utils.get_phase_mapping(sim_mat, sim_thold)
 
@@ -101,16 +95,16 @@ def get_phase_timeline():
     ]
 
     timeline_ret = utils.apply_middleware(middlewares, info['timeline'])
-    response = app.response_class(
-        response=json.dumps(timeline_ret), status=200, mimetype='application/json')
-    return response
+    request.setHeader('Content-Type', 'application/json')
+    request.setHeader('Cache-Control', 'no-store')
+    return json.dumps(timeline_ret)
 
 
 @app.route('/phase/<int:phase_id>/treemap', methods=['POST'])
 @timed
-def get_phase_treemap(phase_id):
+def get_phase_treemap(request, phase_id):
     info = processes['default_']['info']
-    (sim_mat, sim_thold) = get_similarity_params(flask.request)
+    (sim_mat, sim_thold) = get_similarity_params(request)
     # Create new phase list according to the inputs
     mapping_table = utils.get_phase_mapping(sim_mat, sim_thold)
 
@@ -121,16 +115,16 @@ def get_phase_treemap(phase_id):
     ]
 
     treemap_ret = utils.apply_middleware(middlewares, info['phase'])
-    response = app.response_class(
-        response=json.dumps(treemap_ret), status=200, mimetype='application/json')
-    return response
+    request.setHeader('Content-Type', 'application/json')
+    request.setHeader('Cache-Control', 'no-store')
+    return json.dumps(treemap_ret)
 
 
 @app.route('/phase/<int:phase_id>/prof', methods=['POST'])
 @timed
-def get_phase_prof(phase_id):
+def get_phase_prof(request, phase_id):
     info = processes['default_']['info']
-    (sim_mat, sim_thold) = get_similarity_params(flask.request)
+    (sim_mat, sim_thold) = get_similarity_params(request)
     # Create new phase list according to the inputs
     mapping_table = utils.get_phase_mapping(sim_mat, sim_thold)
 
@@ -140,16 +134,16 @@ def get_phase_prof(phase_id):
     ]
 
     counters = utils.apply_middleware(middlewares, info['phase'])
-    response = app.response_class(
-        response=json.dumps(counters), status=200, mimetype='application/json')
-    return response
+    request.setHeader('Content-Type', 'application/json')
+    request.setHeader('Cache-Control', 'no-store')
+    return json.dumps(counters)
 
 
 @app.route('/phase/<int:phase_id>/codes', methods=['POST'])
 @timed
-def get_phase_code(phase_id):
+def get_phase_code(request, phase_id):
     info = processes['default_']['info']
-    (sim_mat, sim_thold) = get_similarity_params(flask.request)
+    (sim_mat, sim_thold) = get_similarity_params(request)
     # Create new phase list according to the inputs
     mapping_table = utils.get_phase_mapping(sim_mat, sim_thold)
 
@@ -159,15 +153,9 @@ def get_phase_code(phase_id):
     ]
 
     codes = utils.apply_middleware(middlewares, info['phase'])
-    response = app.response_class(
-        response=json.dumps(codes), status=200, mimetype='application/json')
-    return response
-
-
-# In order to active flask-profiler, you have to pass flask
-# app as an argument to flask-profiler.
-# All the endpoints declared so far will be tracked by flask-profiler.
-flask_profiler.init_app(app)
+    request.setHeader('Content-Type', 'application/json')
+    request.setHeader('Cache-Control', 'no-store')
+    return json.dumps(codes)
 
 
 def get_descriptions():
@@ -195,48 +183,37 @@ def main(argv):
         type=str,
         nargs='?',
         default=DEFAULT_INPUT_PATH,
-        help='The path to Snippits output. Defaults to {}'.format(DEFAULT_INPUT_PATH))
+        help='The path to Snippits output which contains profiling counters. '
+        'Defaults to {}'.format(DEFAULT_INPUT_PATH))
     ap.add_argument(
         '--host',
         type=str,
-        default=DEFAULT_HOST,
-        help='The host ip address on which Snippit webserver serve. Defaults to {}'.format(
-            DEFAULT_HOST))
+        default=app.config['HOST'],
+        help='The host ip address on which Snippit webserver serve. '
+        'Defaults to {}'.format(app.config['HOST']))
     ap.add_argument(
         '--port',
         '-p',
         type=int,
-        default=DEFAULT_PORT,
-        help='The port on which Snippit webserver will be hosted. Defaults to {}'.format(
-            DEFAULT_PORT))
+        default=app.config['PORT'],
+        help='The port on which Snippit webserver will be hosted. '
+        'Defaults to {}'.format(app.config['PORT']))
     args = ap.parse_args()
 
     input_path = os.path.abspath(args.input_path)
     proc_path = os.path.join(input_path, 'proc')
-    if (os.path.exists(proc_path) == False):
+    if os.path.exists(proc_path) == False:
         print('{} path \'{}\' not found!'.format(bcolors.WARNING_STR, proc_path))
 
     global processes
     processes = snippit.load(proc_path)
-    if (len(processes) == 0):
+    if len(processes) == 0:
         print('{} no processes found under \'{}\'!'.format(bcolors.WARNING_STR, proc_path))
 
-    print(' * Server running on: {}http://{}:{}/{}'.format(bcolors.OKBLUE, args.host, args.port,
-                                                           bcolors.ENDC))
+    print(' * Server running on: '
+          '{}http://{}:{}/{}'.format(bcolors.OKBLUE, args.host, args.port, bcolors.ENDC))
 
-    # Set a minimum log level
-    logzero.loglevel(logging.INFO)
-    if (app.config['DEBUG']):
-        print(' * Profiling results on: {}http://{}:{}/flask-profiler/{}'.format(
-            bcolors.OKBLUE, args.host, args.port, bcolors.ENDC))
-        auth = app.config['flask_profiler']['basicAuth']
-        print('       username: {}    password: {}'.format(auth['username'], auth['password']))
-        logzero.loglevel(logging.DEBUG)
-
-    if (app.config['DEBUG']):
-        app.run(host=args.host, port=args.port, use_debugger=True, use_reloader=True)
-    else:
-        app.run(host=args.host, port=args.port)
+    app.run(host=args.host, port=args.port)
 
 
 if __name__ == '__main__':
