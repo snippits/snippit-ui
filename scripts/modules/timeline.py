9d9a573b54a1b3011435b5b5759027f179c005fb
Medicine Yeh <medicinehy@gmail.com>
2017-11-15 23:15:54 +0800

    Remove deprecated functions

--- scripts/modules/timeline.py
+++ scripts/modules/timeline.py
@@ -36,16 +36,3 @@ def remap(phase_timeline: list, mapping_table: list) -> list:
     '''
     ret = [[kv[0], apply_mapping(mapping_table, kv[1])] for kv in phase_timeline]
     return Hashable(ret)
-
-
-@lru_cache(maxsize=32)
-def append_event(phase_timeline: list, event_list: list, event: int) -> list:
-    ret = [[kv[0], None] for kv in event_list if kv[1] == event]
-    return Hashable(phase_timeline + ret)
-
-
-def append_events_in_order(phase_timeline: list, event_list: list, events: list) -> list:
-    time_list = [kv for kv in event_list if kv[1] in events]
-    # Merge two timeline in sorted order
-    ret = sorted(phase_timeline + time_list)
-    return ret
