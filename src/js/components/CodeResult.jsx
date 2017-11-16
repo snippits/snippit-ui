459eb0460724d97f73d7cf53a5c1326c2a7d2364
Medicine Yeh <medicinehy@gmail.com>
2017-11-16 17:51:14 +0800

    Add simple button to switch showing only walked codes

--- src/js/components/CodeResult.jsx
+++ src/js/components/CodeResult.jsx
@@ -16,11 +16,12 @@ export default class CodeResult extends React.Component {
         super();
         this.state = {
             selectedFile: '',
+            showOnlyWalked: false,
             content: [],
-            codes: [],
         };
 
         this.handleSelectFile = this.handleSelectFile.bind(this);
+        this.toggleContent = this.toggleContent.bind(this);
     }
 
     componentDidMount() {
@@ -31,8 +32,28 @@ export default class CodeResult extends React.Component {
         const selectedFile = e.target.value;
 
         this.setState({selectedFile: selectedFile});
-        this.props.dispatch(fetchFile(selectedFile, (content) => {
-            const lookupMapping = this.props.code.data
+        dispatch(fetchFile(selectedFile, (content) => {
+            this.setState({content: content});
+        }));
+    }
+
+    toggleContent(e) {
+        this.setState({showOnlyWalked: !this.state.showOnlyWalked});
+    }
+
+    render() {
+        const {code} = this.props;
+        const {files} = this.props;
+        let codes = code.data;
+
+        console.log('CodeResult');
+
+        if (code.error) {
+            console.log('Cannot fetch the codes');
+            codes = [];
+        }
+        if (this.state.selectedFile !== '') {
+            const lookupMapping = codes
                 .map((lineInfo) => {
                     if (lineInfo.line.startsWith(this.state.selectedFile)) {
                         const tmp = lineInfo.line.split(':');
@@ -49,28 +70,18 @@ export default class CodeResult extends React.Component {
                 lookupTable[line] = {walk: walk};
             }
 
-            const codes = content.map((line, idx) => {
-                if (lookupTable[idx]) return {line: line, walk: lookupTable[idx].walk};
-                return {line: line, walk: null};
-            });
-
-            this.setState({content: content, codes: codes});
-        }));
-    }
-
-    render() {
-        const {code} = this.props;
-        const {files} = this.props;
-        let codes = code.data;
-
-        console.log('CodeResult');
-
-        if (code.error) {
-            console.log('Cannot fetch the codes');
-            codes = [];
-        }
-        if (this.state.selectedFile !== '') {
-            codes = this.state.codes;
+            codes = this.state.content
+                .map((line, idx) => {
+                    if (lookupTable[idx]) {
+                        return {lineNum: idx, line: line, walk: lookupTable[idx].walk};
+                    }
+                    if (!this.state.showOnlyWalked) {
+                        return {lineNum: idx, line: line, walk: undefined};
+                    }
+                })
+                .filter((element) => {
+                    return element !== undefined;
+                });
         }
 
         // This is used to force clean all browser implicit state and rebuild component
@@ -79,7 +90,10 @@ export default class CodeResult extends React.Component {
             <div className='col-md-6'>
                 <h1 className='pure-u-1-1'>Code</h1>
                 <div>
-                    <select className='select_box col-md-12'
+                    <input type='button' className='col-md-1 btn btn-primary'
+                        onClick={this.toggleContent}>
+                    </input>
+                    <select className='select_box col-md-11'
                         onChange={this.handleSelectFile} value={this.state.selectedFile}>
 
                         <option key='no-select' value=''></option>
@@ -102,7 +116,7 @@ class HighlightedCode extends React.Component {
             return values.map((code, i) =>
                 <tr key={i} className='no-border'>
                     <td className='col-md-1 walk-count'>{code.walk}</td>
-                    <td className='line-num'>{i}</td>
+                    <td className='line-num'>{code.lineNum || i}</td>
                     <td className='code'>{code.line + '\n'}</td>
                 </tr>
             );
